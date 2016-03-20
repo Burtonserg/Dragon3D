501ab921c2dd6fd70e0e4d93e959a5a3109fd630
Yaroslav <serov.m1976@mail.ru>
2016-03-20 22:49:59 +0200

    Added files via upload

---
+++ Player2.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+public class Player2 : MonoBehaviour
+{
+    public float rotationSpeed = 60;
+    void Start()
+    {
+        Transform current = transform;
+        for (int i = 0; i < 2; i++)
+        {
+            
+            tail tail = GameObject.CreatePrimitive(PrimitiveType.Cube).AddComponent<tail>();            
+            
+            tail.transform.position = current.transform.position - current.transform.forward * 2;
+            
+            tail.transform.rotation = transform.rotation;
+            
+            tail.target = current.transform;
+
+            tail.targetDistance = 2;
+            
+            Destroy(tail.GetComponent<Collider>());
+            
+            current = tail.transform;
+            //float horizontal = Input.GetAxis("Horizontal");
+           // tail.transform.Rotate(10, rotationSpeed * Time.deltaTime * horizontal, 10);
+        }
+    }    
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * 10);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.down);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(Vector3.right);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(Vector3.left);
+        }
+    }
+}
